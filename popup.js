fb6c6fbe11cebb364cfac7c7ff44ea9370bee10a
igelman <alan@igelman.com>
2016-05-18 21:01:41 -0400

    Refactor tr element loop to a function in popup.js

--- popup.js
+++ popup.js
@@ -3,7 +3,15 @@
 */
 var seleneUrl = "http://nyselene1.ops.about.com:8080/";
 var taxeneEndpoint = "taxene/children/";
-var queryParameters = "childrenNodeTypes=TAXONOMY&childrenNodeTypes=DOCUMENT&isRecursive=false&includeDocumentSummaries=true&includeConfigs=true";
+var taxeneQueryParameters = "childrenNodeTypes=TAXONOMY&childrenNodeTypes=DOCUMENT&isRecursive=false&includeDocumentSummaries=true&includeConfigs=true";
+
+/*
+	Config settings for Solr api.	
+*/
+var solrUrl = "http://nyprsolr-read.ops.about.com:8983/";
+var solrEndpoint = "solr/cmsDocs_rep/select";
+var solrQueryParameters = "q=vertical%3AHEALTH%0Astate%3AACTIVE%0AupdatedDate%3A%5BNOW%2FDAY+TO+*%5D&sort=updatedDate+desc&fl=docId%2Curl%2Cstate%2CtemplateType%2CupdatedDate%2CdirName%2Cchannel%2Ctitle&wt=json&indent=true";
+// ?
 
 // Kick off the whole thing after the popup loads.
 document.addEventListener('DOMContentLoaded', function(){
@@ -32,7 +40,7 @@ function getCurrentTabUrl(callback) {
 // GET the children from Selene (and print to a table).
 function doTheWholeThing (url) {
 	var nodeId = extractNodeId(url);
-	getSeleneJson( constructApiUrl(nodeId) );
+	getSeleneJson( constructSeleneUrl(nodeId) );
 }
 
 // DocId is (almost) always the numeric string following the last hyphen in the url.
@@ -43,9 +51,9 @@ function extractNodeId(url) {
     return nodeId;
 }
 
-function constructApiUrl(docId) {
+function constructSeleneUrl(docId) {
 	var apiUrl = "";
-	apiUrl += seleneUrl + taxeneEndpoint + docId + "?" + queryParameters;
+	apiUrl += seleneUrl + taxeneEndpoint + docId + "?" + taxeneQueryParameters;
 	console.log("function constructApiUrl\n" + "apiUrl: " + apiUrl);
 	return apiUrl;
 }
@@ -58,14 +66,13 @@ function getSeleneJson(apiUrl) {
 		table += "<th>docId</th><th>Type</th><th>Weight</th><th>Slug</th>";
 		table += "</tr></thead><tbody>";
 		data.data.children.list.forEach( function(item, index) {
-			var tr = "<tr>";
-			tr += "<td>" + item.docId + "</td>";
-			tr += "<td>" + item.nodeType.toLowerCase() + "</td>";
-			tr += "<td>" + item.primaryParentWeight + "</td>";
-			tr += "<td>" + "<a target='_blank' href='" + item.document.url + "'>" + item.document.slug + "</a>" + "</td>";
-			tr +=  "</tr>";
-			table += tr;
-			//$( '#data' ).append(index + ": " + item.docId + "<br>");
+			var cellArray = [
+				item.docId,
+				item.nodeType.toLowerCase(),
+				item.primaryParentWeight,	
+				"<a target='_blank' href='" + item.document.url + "'>" + item.document.slug + "</a>"
+			];
+			table += makeTr(cellArray);
 		});
 		table += "</tbody></table>";
 		$( '#data' ).append(table);
@@ -73,3 +80,13 @@ function getSeleneJson(apiUrl) {
 	});
 }
 
+function makeTr(cellArray) {
+	var tr = "<tr>";
+	cellArray.forEach ( function(item, index) {
+		tr += "<td>" + item + "</td>";
+	});
+	tr += "</tr>";
+	return tr;
+}
+
+// http://nyprsolr-read.ops.about.com:8983/solr/cmsDocs_rep/select?q=vertical%3AHEALTH%0Astate%3AACTIVE%0AupdatedDate%3A%5BNOW%2FDAY+TO+*%5D&sort=updatedDate+desc&fl=docId%2Curl%2Cstate%2CtemplateType%2CupdatedDate%2CdirName%2Cchannel%2Ctitle&wt=json&indent=true
